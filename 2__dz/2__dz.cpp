514197a2217219145cd7c108d22f85a50f81b084
Hellhound <hardoneloves@gmail.com>
2021-11-25 20:50:21 +0300

    Добавьте файлы проекта.

---
+++ 2__dz/2__dz.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main(){
+	int a[10];
+	for (int i = 0; i < 10; i++) {
+		std::cin >>a[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		if (a[i] == 0) a[i] = 1;
+		else a[i] = 0;
+	}
+	for (int i = 0; i < 10; i++) {
+		std::cout << a[i] << " ";
+	}
+}
